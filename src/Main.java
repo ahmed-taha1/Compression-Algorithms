0f854ae8092e7210e906cac0cf81ca9dff746832
ahmed taha <bjabotaha@yahoo.com>
2023-10-25 00:18:38 +0300

    compress functions and ui added

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final UI ui = new UI();
+        ui.run();
+    }
+}
+
+/*
+    test 1#:
+        input:
+            cabracadabrarrarrad
+        output:
+            <0, 0, c>
+            <0, 0, a>
+            <0, 0, b>
+            <0, 0, r>
+            <3, 1, c>
+            <2, 1, d>
+            <7, 4, r>
+            <3, 3, r>
+            <3, 1, d>
+
+    test 2#:
+        input:
+            abracadabrad
+        output:
+            <0, 0, a>
+            <0, 0, b>
+            <0, 0, r>
+            <3, 1, c>
+            <2, 1, d>
+            <7, 4, d>
+
+    test 3#:
+        input:
+            ABAABABAABBBBBBBBBBBBA
+        output:
+            <0, 0, A>
+            <0, 0, B>
+            <2, 1, A>
+            <3, 2, B>
+            <5, 3, B>
+            <2, 2, B>
+            <5, 5, B>
+            <1, 1, A>
+*/
---
+++ src/tag.java
@@ -0,0 +1,9 @@
+public class tag {
+    public tag(int position, int length, char next){
+        this.position = position;
+        this.length = length;
+        this.next = next;
+    }
+    public int position, length;
+    public char next;
+}
